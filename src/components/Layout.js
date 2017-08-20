003f70e04deaab5f080765c5a84e089ab7d25096
Tobias Huonder <tobias.c.huonder@student.uts.edu.au>
2017-08-20 23:01:38 +1000

    refactoring

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,35 +6,11 @@ import Header from './Layout/Header';
 
 
 export default class Layout extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      title: "Hoi Fritz",
-    };
-  }
-
-  changeTitle(title) {
-    this.setState({title});
-  }
-
-  getServerData(){
-    const url = 'http://localhost:3001'
-    fetch(url)
-    .then(function(response) {
-    console.log(response);
-})
-  }
   render() {
-    this.getServerData();
-    const movie = {
-      title: "The Rock",
-      dureation: "94",
-      genre: "Action",
-    };
     return (
       <div>
-        <Header changeTitle={this.changeTitle.bind(this)} title ={this.state.title}/>
-        <Body movie={movie} />
+        <Header />
+        <Body />
         <Footer />
       </div>
     );
