b8c198b7229e175bb3ffd905145a6a7c0dd1013e
Simon Mueller <13031695@student.uts.edu.au>
2017-08-21 12:42:17 +1000

    refactorings and cleanups

--- src/components/Layout/Body/MovieGrid/Movie.js
+++ src/components/Layout/Body/MovieGrid/Movie.js
@@ -1,10 +1,32 @@
 import React from 'react';
 
 export default class Movie extends React.Component {
+  constructor(){
+    super();
+
+    this.handleMovieChecked = this.handleMovieChecked.bind(this);
+  }
+
+  handleMovieChecked() {
+    const that = this;
+    const url = 'http://localhost:3001';
+
+    const headers = new Headers({
+      "Content-Type": "application/json"
+    });
+
+    fetch(url, {
+      method: 'post',
+      headers: headers,
+      body: JSON.stringify({title: that.props.movieTitle})
+    });
+  }
+
   render() {
     return (
       <div id="movie">
         <h3> {this.props.movieTitle} </h3>
+        <button onClick={this.handleMovieChecked}>Mark seen</button>
       </div>
     );
   }
