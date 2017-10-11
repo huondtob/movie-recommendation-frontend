2cb86d4168175c59e35cdba31cdc7a2fea850faf
Simon Mueller <13031695@student.uts.edu.au>
2017-10-11 22:28:49 +1100

    minor changes to movie rendering

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 export default function Movies(props) {
   const { moviesError, movies, handleChange } = props;
-  console.log(movies);
+
+  const movieContainers = movies.map(movie => (<div key={movie}>{movie}</div>));
+
   return (
     <div>
       <h1>Movies</h1>
       <input type="text" onChange={handleChange} />
-      <p>{ JSON.stringify(movies) }</p>
+
+      { movieContainers }
       { moviesError && <strong>{ moviesError.message }</strong> }
     </div>
   );
--- src/components/Recommendations.js
+++ src/components/Recommendations.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 export default function Recommendations(props) {
+  const { moviesError } = props;
+
   return (
     <div>
       <h1>Recommendations</h1>
+      { moviesError && <strong>{ moviesError.message }</strong> }
     </div>
   );
 }
